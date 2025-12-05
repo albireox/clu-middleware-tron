b4d9ced7034be7f7f0ccc548cc49a58c7dd367b2
José Sánchez-Gallego <gallegoj@uw.edu>
2025-12-05 10:11:43 -0800

    Add crate-level documentation to src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,12 @@
  *  @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
  */
 
+//! A library and service to act as middleware between Tron actors and a RabbitMQ message broker.
+//!
+//! Refer to the [README](https://github.com/albireox/clu-middleware-tron/blob/main/README.md) in
+//! the [clu-middleware-tron](https://github.com/albireox/clu-middleware-tron) repository for more
+//! information.
+
 pub mod parser;
 pub mod rabbitmq;
 pub mod tcp;
